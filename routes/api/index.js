0393e2bcbc0b81ee48774711ac6ff8e3669669be
Alan Leverenz <awleverenz@aol.com>
2019-12-19 12:10:56 -0500

    inserted the presentation page

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,12 @@
 var router = require("express").Router();
 var fetchRoutes = require("./fetch");
-var noteRoutes = require("./notes");
+// var noteRoutes = require("./notes");
 var headlineRoutes = require("./headlines");
 var clearRoutes = require("./clear");
 var fetchController = require("../../controllers/fetch");
 
 router.use("/fetch/:query", fetchController.getNews);
-router.use("/notes", noteRoutes);
+// router.use("/notes", noteRoutes);
 router.use("/headlines", headlineRoutes);
 router.use("/clear", clearRoutes);
 
--- routes/view/index.js
+++ routes/view/index.js
@@ -4,19 +4,15 @@ var db = require("../../models");
 // This route renders the homepage
 router.get("/", function(req, res) {
   db.Headline.find({ saved: false })
-    .sort({ date: -1 })
+    .sort({ score: 1 })
     .then(function(dbArticles) {
       res.render("home", { articles: dbArticles });
     });
 });
 
 // This route renders the saved handlebars page
-router.get("/saved", function(req, res) {
-  db.Headline.find({ saved: true })
-    .sort({ date: -1 })
-    .then(function(dbArticles) {
-      res.render("saved", { articles: dbArticles });
-    });
+router.get("/presentation", function(req, res) {
+    res.render("presentation");
 });
 
 module.exports = router;
