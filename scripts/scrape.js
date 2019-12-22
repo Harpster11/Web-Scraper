402da8ed12fd8c8c059616eeec3c0b77164ca4db
Harpster11 <52804898+Harpster11@users.noreply.github.com>
2019-12-22 13:54:41 -0600

    Update scrape.js

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,86 +1,47 @@
-// require axios and cheerio, enabling scrapes
+// require axios
 var axios = require("axios");
 const NewsAPI = require('newsapi');
-// const newsapi = new NewsAPI ('4acc3b9a971c4bf0ab6b2c7117303592');
 const newsapi = new NewsAPI('bfc8e374d6df45af85688db28a5bf373');
-
-var articles = [];
-
-function getNews(newsSearch){
-    console.log("before newsApi")
-
-newsapi.v2.everything({
-        q: newsSearch,
-        language: 'en',
-      }).then(response => {
-        
-        console.log("/n/nthen")
-          articles = response.articles.slice(0,5);
-          // console.log("Article Array:", articles);
-          let promises =[];
-
-          articles.forEach(article => {
-              promises.push(axios({
-                  "method":"GET",
-                  "url":"https://twinword-sentiment-analysis.p.rapidapi.com/analyze/",
-                  "headers":{
-                  "content-type":"application/x-www-form-urlencoded",
-                  "x-rapidapi-host":"twinword-sentiment-analysis.p.rapidapi.com",
-                //   "x-rapidapi-key":"bcbc7d6dd8msh5e1eb73a59e842fp1df3fcjsnd9394db0f416"
-                  "x-rapidapi-key":"ab0401ba39mshd22cfe38b5efc58p146bfbjsnd2d275948b9e"
-
-                  },"params":
-                      {
-                      "text": article.content
-                      }
-                  }))
-          })
-          Promise.all(promises)
-              .then(responses => {
-                  articles.map((article, i) => {
-                      let keys = Object.keys(responses[i].data)
-                      keys.forEach(key => {
-                          if (key !== "keywords") {
-                          article[key] = responses[i].data[key]
-                          }
-                      })
-                      return article
-                  })
-
-                  console.log("articles return",articles);
-                  return(articles)
-
-              })
-              .catch((error)=>{
-                  console.log(error)
-              });
-
-              
-      
-          // for (var i=0; i<5; i++) {
-          //     result.push()
-          //     console.log("[i] ===== TWINWORD ======= ");
-          //     axios({
-          //         "method":"GET",
-          //         "url":"https://twinword-sentiment-analysis.p.rapidapi.com/analyze/",
-          //         "headers":{
-          //         "content-type":"application/x-www-form-urlencoded",
-          //         "x-rapidapi-host":"twinword-sentiment-analysis.p.rapidapi.com",
-          //         "x-rapidapi-key":"bcbc7d6dd8msh5e1eb73a59e842fp1df3fcjsnd9394db0f416"
-          //         },"params":
-          //             {
-          //             "text": content
-          //             }
-          //         })
-          //         .then((response)=>{
-          //             console.log(response.data)
-          //         })
-          //         .catch((error)=>{
-          //             console.log(error)
-          //         });
-          //     };
-          });
-        }
-          
-        
-module.exports = getNews;
+var articles = [];function scrape(input) {    const scrapePromise = new Promise((resolve, reject) => {        newsapi.v2.everything({
+            q: input,
+            language: 'en',
+        }).then(response => {
+            // console.log("Article Array:", response);
+            articles = response.articles.slice(0, 5);
+            // console.log("Article Array:", articles);
+            let promises = [];
+            articles.forEach(article => {
+                promises.push(axios({
+                    "method": "GET",
+                    "url": "https://twinword-sentiment-analysis.p.rapidapi.com/analyze/",
+                    "headers": {
+                        "content-type": "application/x-www-form-urlencoded",
+                        "x-rapidapi-host": "twinword-sentiment-analysis.p.rapidapi.com",
+                        "x-rapidapi-key": "bcbc7d6dd8msh5e1eb73a59e842fp1df3fcjsnd9394db0f416"
+                    }, "params":
+                    {
+                        "text": article.content
+                    }
+                }))
+            }
+            )
+            Promise.all(promises).then(responses => {
+                articles.map((article, i) => {
+                    let keys = Object.keys(responses[i].data)
+                    keys.forEach(key => {
+                        if (key !== "keywords") {
+                            article[key] = responses[i].data[key]
+                        }
+                    })
+                    return article
+                })
+                console.log(articles)
+                resolve(articles)
+            })
+                .catch((error) => {
+                    reject(error);
+                });
+        });
+    });
+    return scrapePromise
+}module.exports = scrape;
